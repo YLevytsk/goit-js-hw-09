8ef7b4489a25fa92e756d1b4f0c3d5bd68f5fc07
yLevytsk <Lupin773311asd@gmail.com>
2025-01-26 19:56:26 +0000

    ввпв

---
+++ src/js/2-form.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".feedback-form");
+    const STORAGE_KEY = "feedback-form-state";
+    let formData = { email: "", message: "" };
+
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        formData = JSON.parse(savedData);
+        form.email.value = formData.email;
+        form.message.value = formData.message;
+    }
+
+    form.addEventListener("input", (event) => {
+        formData[event.target.name] = event.target.value.trim();
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    });
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (!form.email.value || !form.message.value) {
+            alert("Fill please all fields");
+            return;
+        }
+        console.log(formData);
+        localStorage.removeItem(STORAGE_KEY);
+        form.reset();
+        formData = { email: "", message: "" };
+    });
+});
