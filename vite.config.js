e887d0fd20a04ebab172f53778c9cddbd8625e6a
yLevytsk <Lupin773311asd@gmail.com>
2025-01-26 17:47:31 +0000

    gf

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,48 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
-import FullReload from 'vite-plugin-full-reload'; // Оставьте если нужно, чтобы следить за изменениями в HTML
-import SortCss from 'postcss-sort-media-queries'; // Это зависит от ваших предпочтений
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    // Убираем pлагин для инжектирования HTML
-    root: 'src/public', // Задаем путь к папке с HTML файлами
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/public/*.html'), // Входные HTML файлы
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: '[name].js', // Файлы JS будут иметь имя страницы
-          assetFileNames: 'assets/[name]-[hash][extname]', // Статические ресурсы
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
-      outDir: '../../dist', // Выводим билд в папку dist на уровень выше
-      emptyOutDir: true, // Очищаем папку перед сборкой
+      outDir: '../dist',
+      emptyOutDir: true,
     },
     plugins: [
-      FullReload(['./src/public/**/**.html']), // Плагин для перезагрузки при изменениях в HTML
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
       SortCss({
-        sort: 'mobile-first', // Сортировка CSS по принципу мобильной адаптивности
+        sort: 'mobile-first',
       }),
     ],
   };
 });
-
