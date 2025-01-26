a62e2bb464af80d06ff7e8aea8e5b373cf39df05
yLevytsk <Lupin773311asd@gmail.com>
2025-01-26 21:48:33 +0000

    роап

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const STORAGE_KEY = "feedback-form-state";
     let formData = { email: "", message: "" };
 
+    // Проверяем, есть ли сохраненные данные в localStorage
     const savedData = localStorage.getItem(STORAGE_KEY);
     if (savedData) {
         formData = JSON.parse(savedData);
@@ -10,20 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
         form.message.value = formData.message;
     }
 
+    // Слушатель на изменения в форме
     form.addEventListener("input", (event) => {
         formData[event.target.name] = event.target.value.trim();
         localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     });
 
+    // Слушатель на отправку формы
     form.addEventListener("submit", (event) => {
         event.preventDefault();
+
+        // Проверка на заполненность всех полей
         if (!form.email.value || !form.message.value) {
             alert("Fill please all fields");
             return;
         }
+
         console.log(formData);
-        localStorage.removeItem(STORAGE_KEY);
-        form.reset();
-        formData = { email: "", message: "" };
+        localStorage.removeItem(STORAGE_KEY);  // Убираем сохраненные данные
+        form.reset();  // Очищаем форму
+        formData = { email: "", message: "" };  // Сброс данных
     });
 });
+
